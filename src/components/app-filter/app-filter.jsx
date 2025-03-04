4f6dbac12485f838a696a0db112625bc45b56c99
Olegoriy <olegkib1506@gmail.com>
2025-03-04 15:58:40 +0300

    add: ready layout

--- src/components/app-filter/app-filter.jsx
+++ src/components/app-filter/app-filter.jsx
@@ -2,7 +2,7 @@ import './app-filter.css'
 
 const AppFilter = () => {
     return (
-        <div className="btn-grup">
+        <div className="btn-group">
             <button className="btn btn-light" type="button">Все сотрудники</button>
             <button className="btn btn-outline-light" type="button">Сотрудники на повышение</button>
             <button className="btn btn-outline-light" type="button">З/П больше 1000$</button>
--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,18 +1,23 @@
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/eployees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css'
 
 function App() {
     return (
         <div className="app">
-            <AppInfo />
+            <AppInfo/>
 
             <div className="search-panel">
-                <SearchPanel />
-                <AppFilter />
+                <SearchPanel/>
+                <AppFilter/>
             </div>
+
+            <EmployeesList/>
+            <EmployeesAddForm/>
         </div>
     );
 }
