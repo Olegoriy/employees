220a16210395078606a22c61be8f3697f3d09cf0
Olegoriy <olegkib1506@gmail.com>
2025-03-05 13:29:16 +0300

    fix: using component's props

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -7,6 +7,12 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css'
 
 function App() {
+
+    const data = [
+        {name: "John C.", salary: 800},
+        {name: "Alex M.", salary: 3000},
+        {name: "Carl W.", salary: 5000}
+    ];
     return (
         <div className="app">
             <AppInfo/>
@@ -16,7 +22,7 @@ function App() {
                 <AppFilter/>
             </div>
 
-            <EmployeesList/>
+            <EmployeesList data={data}/>
             <EmployeesAddForm/>
         </div>
     );
